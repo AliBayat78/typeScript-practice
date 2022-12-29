b70d109e329dcf6bfc4404105333e4134bef9be4
Ali Bayat <ali.bayat77@yahoo.com>
2022-12-29 21:31:14 +0330

    clip 4

---
+++ file.js
@@ -0,0 +1,13 @@
+"use strict";
+class Person {
+    constructor() {
+        this.name = 'ali';
+        this.family = 'bayat';
+        this.age = 234;
+    }
+}
+{
+    let cat = 'meow';
+    let dog = 'haphap';
+    document.write(cat + dog);
+}
---
+++ file.ts
@@ -0,0 +1,16 @@
+class Person {
+  name: string
+  family: string
+  age: number
+
+  constructor() {
+    this.name = 'ali'
+    this.family = 'bayat'
+    this.age = 234
+  }
+}
+{
+  let cat = 'meow'
+  let dog = 'haphap'
+  document.write(cat + dog)
+}
