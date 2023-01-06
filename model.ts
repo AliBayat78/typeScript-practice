35260fe5c8c1a838ff7f084bf1d08dcd010e50d7
Ali Bayat <ali.bayat77@yahoo.com>
2023-01-06 18:52:10 +0330

    namespaces

--- model.ts
+++ model.ts
@@ -1,19 +1,26 @@
-interface Todo {
-  id: number
-  name: string
-  state: TodoState
+namespace TodoApp.Model {
+  export interface Todo {
+    id: number
+    name: string
+    state: TodoState
+  }
 }
 
-enum TodoState {
-  New,
-  Active,
-  Complete,
-  Delete,
+namespace TodoApp.Model {
+  export enum TodoState {
+    New,
+    Active,
+    Complete,
+    Delete,
+  }
 }
 
-interface ITodoService {
+namespace DataAccess {
+  import Todo = TodoApp.Model.Todo
+  interface ITodoService {
     add(todo: Todo): Todo
     delete(todoId: number): void
     getAll(): Todo[]
     getbyId(todoId: number): Todo
+  }
 }
